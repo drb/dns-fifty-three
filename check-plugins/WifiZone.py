d5d93513b112cb06d20eab51f76bc5358fcefffa
Dave Bullough <dave@kaeto.co.uk>
2013-07-04 14:12:25 +0100

    WiFi zone plugin now works on OSX. Main process updates DNS entries based on IP from external service.

--- check-plugins/WifiZone.py
+++ check-plugins/WifiZone.py
@@ -1,29 +1,67 @@
 import subprocess
+import os
+import platform
+
+# Sample data returned from the OSX Airport wifi check
+#
+# agrCtlRSSI: -47
+# agrExtRSSI: 0
+# agrCtlNoise: -88
+# agrExtNoise: 0
+# state: running
+# op mode: station
+# lastTxRate: 145
+# maxRate: 144
+# lastAssocStatus: 0
+# 802.11 auth: open
+# link auth: wpa2-psk
+# BSSID: 0:1d:aa:a2:13:30
+# SSID: Kaeto
 
 # Checks the local connection's SSID
 class WifiZone ():
 
-	#ssid = 'HAL'
+	conf = {}
+	target = 'Kaeto'
 
-    def __init__(self):
+	def __init__(self, conf):
 
-        print '__init__ from plugin', self.__class__.__name__
+		print 'Check plugin loaded', self.__class__.__name__
 
-    def run(self):
+		self.conf = conf
 
-        ls_output = subprocess.check_output(['/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport', '-I'])
+	def run(self):
 
-        output = False
-        tokens = ls_output.split('\n')
+		output = False
 
-        for token in tokens:
+		os_name = os.name
+		platform_system = platform.system()
+		platform_release = platform.release()
 
-        	parts = token.strip()
-        	key, value = parts.split(':', 1)
-        	
-        	if key.strip() == 'SSID':
-        		value = value.strip()
-        		if value == 'HAL':
-        			output = True
+		# OSX Check
+		if 'Darwin' == platform_system:
 
-        return output
+			ls_output = subprocess.check_output(['/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport', '-I'])
+
+			# todo have checks from other systems
+			tokens = ls_output.split('\n')
+
+			for token in tokens:
+
+				parts = token.strip()
+
+				if len(parts) > 0:
+
+					try:
+						key, value = parts.split(':', 1)
+					except Exception, ex:
+						print ex
+									
+					if key.strip().lower() == 'ssid':
+						value = str(value).strip()
+						if str(value) == self.target:
+							output = True
+		else:
+			print '[warning]', self.__class__.__name__, 'does not have a method implemented to extract wifi zone yet'
+
+		return output
--- dns-53-service.py
+++ dns-53-service.py
@@ -1,21 +1,17 @@
-#import boto
-#from boto.route53.record import ResourceRecordSets
-#conn = boto.connect_route53()
-#changes = ResourceRecordSets(conn, "ZXXXXXXXXXXXXXX")
-#change = changes.add_change("CREATE", boto.config.get("dns", "name"),"CNAME")
-#change.add_value(boto.config.get("Instance", "public-hostname"))
-#changes.commit()
-
 import sys
 import os
 import yaml
 import sched
 import time
 import boto
+import urllib2
+import json
+
+from boto.route53.record import ResourceRecordSets
 from daemon import Daemon
 
 checkConfig = {}
-checkConfig['interval'] = 5;
+checkConfig['interval'] = 600 # call every 10 minutes;
 
 class Dns53(Daemon):
 
@@ -24,6 +20,7 @@ class Dns53(Daemon):
 	s = sched.scheduler(time.time, time.sleep)
 
 	def __init__(self, pid):
+
 		# load the config
 		stream = open("53.yaml", 'r')
 		self.conf = yaml.safe_load(stream)
@@ -33,33 +30,31 @@ class Dns53(Daemon):
 		self.loadCheckPlugins(self.conf['pluginDir'])
 
 	# loads the check files into memory
-	def loadCheckPlugins(self, pluginsPath):
+	def loadCheckPlugins(self, plugins_root_path):
 
-		if pluginsPath != '':
+		if plugins_root_path != '':
 
-			if os.access(pluginsPath, os.R_OK) == False:
+			if os.access(plugins_root_path, os.R_OK) == False:
 				print 'Plugin path is set but not readable by agent. Skipping plugins.'
 				return False
 
 		else:
 			return False
 
-		# Have we already imported the plugins?
-		# Only load the plugins once
+		# load the plugins once
 		if self.plugins == None:
 
-			sys.path.append(pluginsPath)
+			sys.path.append(plugins_root_path)
 
 			self.plugins = []
 			plugins = []
 
 			# Loop through all the plugin files
-			for root, dirs, files in os.walk(pluginsPath):
+			for root, dirs, files in os.walk(plugins_root_path):
 				for name in files:
 
 					name = name.split('.', 1)
 
-					# Only pull in .py files (ignores others, inc .pyc files)
 					try:
 						if name[1] == 'py':
 
@@ -70,24 +65,24 @@ class Dns53(Daemon):
 						continue
 
 			# Loop through all the found plugins, import them then create new objects
-			for pluginName in plugins:
+			for plugin_name in plugins:
 				
-				pluginPath = os.path.join(pluginsPath, '%s.py' % pluginName)
+				plugin_path = os.path.join(plugins_root_path, '%s.py' % plugin_name)
 
-				if os.access(pluginsPath, os.R_OK) == False:
-					print 'Unable to read dir so skipping this plugin.', pluginsPath
+				if os.access(plugins_root_path, os.R_OK) == False:
+					print 'Unable to read dir so skipping this plugin.', plugins_root_path
 					continue
 
 				try:
 					# Import the plugin, but only from the pluginDirectory (ensures no conflicts with other module names elsehwhere in the sys.path
 					import imp
-					importedPlugin = imp.load_source(pluginName, pluginPath)
+					importedPlugin = imp.load_source(plugin_name, plugin_path)
 
 					# Find out the class name and then instantiate it
-					pluginClass = getattr(importedPlugin, pluginName)
+					pluginClass = getattr(importedPlugin, plugin_name)
 
 					try:
-						pluginObj = pluginClass()
+						pluginObj = pluginClass(self.conf)
 
 					except TypeError:
 
@@ -97,7 +92,7 @@ class Dns53(Daemon):
 					self.plugins.append(pluginObj)
 
 				except Exception, ex:
-					import traceback
+					print 'Error loading check plugin', ex
 
 		# Now execute the objects previously created
 		if self.plugins != None:
@@ -111,33 +106,99 @@ class Dns53(Daemon):
 					output[plugin.__class__.__name__] = plugin.run()
 
 				except Exception, ex:
-					import traceback
+					print 'Error running plugin', ex
 
-				print output
-				#self.mainLogger.debug('getPlugins: %s output: %s', plugin.__class__.__name__, output[plugin.__class__.__name__])
-				#self.mainLogger.info('getPlugins: executed %s', plugin.__class__.__name__)
+				print plugin.__class__.__name__, 'output', output
 
-			#self.mainLogger.debug('getPlugins: returning')
-
-			# Each plugin should output a dictionary so we can convert it to JSON later
+			# Each plugin needs to return True to fire the DNS update request
 			return output
 
 		else:
-			print 'getPlugins: no plugins, returning false'
 
 			return False
 
 
+	# Schedules the next check
 	def setNextCheck(self):
 		self.s.enter(checkConfig['interval'], 1, self.doChecks, (self.s, "a message"))
 
+	# daemon entry
 	def run(self):
 		self.setNextCheck();
 		self.s.run()
 
 	# Does the checks against the prerequisites
 	def doChecks(self, sched, msg):
-		print 'Doing checks', self.conf['zoneId']
+
+		checks_passed = True
+
+		plugin_checks = self.loadCheckPlugins(self.conf['pluginDir'])
+
+		for plugin_name, result in plugin_checks.items():
+			print plugin_name, result
+			if result == False:
+				checks_passed = False
+
+		if checks_passed:
+
+			f = urllib2.urlopen(self.conf['ipResolver']).read()
+			j = json.loads(f)
+
+			ip = j['client_ip']
+			host_name = self.conf['recordName']
+
+			current_record = None
+			conn = boto.connect_route53()
+
+			# dict of all entries for this zone
+			existing_entries = conn.get_all_rrsets(self.conf['zoneId'])
+
+
+			# find the target cname in the entries
+			for x in existing_entries:
+				host_name_period = host_name
+				if host_name[-1] != '.':
+					host_name_period = host_name_period + '.'
+
+				if x.name == host_name_period:
+					print "i found ", host_name, x.resource_records
+					current_record = x
+					break
+				else:
+					x = None
+
+			# a matching record was found, so test it against the IP we retrieved form the webservice and check if it needs an update
+			if x != None:
+
+				if ip in current_record.resource_records:
+					print 'no change required'
+				else:
+					print 'need to update', current_record.resource_records, 'to', ip
+
+					changes = ResourceRecordSets(conn, self.conf['zoneId'])
+
+					# rmeove the old record first (passing in the existing ip, otherwise this won't work)
+					change = changes.add_change("DELETE", host_name, "A")
+					change.add_value(','.join(current_record.resource_records))
+					
+					# now recreate the record with the new ip
+					change = changes.add_change("CREATE", host_name, "A")
+					change.add_value(ip)
+
+					#check the result
+					result = changes.commit()
+
+			# if it needs to be created, do it now
+			else:
+				print host_name, 'record needs to be created'
+
+				changes = ResourceRecordSets(conn, self.conf['zoneId'])
+
+				change = changes.add_change("CREATE", host_name, "A")
+				change.add_value(ip)
+				result = changes.commit()
+
+		# schedule the next check
 		self.setNextCheck()
 
 check = Dns53('/dev/null/pid.pid')
